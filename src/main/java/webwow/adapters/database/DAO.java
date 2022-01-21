6335e8c86347259228b10ec4ad994c2352e60e13
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2022-01-21 18:42:08 +0000

    Move to db with SERIAL id

--- src/main/java/webwow/adapters/database/DAO.java
+++ src/main/java/webwow/adapters/database/DAO.java
@@ -21,7 +21,7 @@ public class DAO {
     public int nextID() {
         int result = -1;
         try (Statement statement = databaseConnection.createStatement()) {
-            String query = "select max(\"ID\") from \"Albums\";";
+            String query = "SELECT MAX(id) FROM album;";
             ResultSet resultSet = statement.executeQuery(query);
             resultSet.next();
             result = resultSet.getInt(1);
@@ -43,7 +43,7 @@ public class DAO {
     }
 
     private List<AlbumModel> tryToGetAllAlbums(Statement statement) throws SQLException {
-        String query = "select * from \"Albums\" order by \"Artist\", \"Year\", \"Name\";";
+        String query = "SELECT * FROM album ORDER BY artist, year, name;";
         ResultSet result = statement.executeQuery(query);
         return getAlbumsFromResultSet(result);
     }
@@ -51,10 +51,10 @@ public class DAO {
     private List<AlbumModel> getAlbumsFromResultSet(ResultSet result) throws SQLException {
         List<AlbumModel> albums = new ArrayList<>();
         while (result.next()) {
-            int id = result.getInt("ID");
-            String name = result.getString("Name");
-            String artist = result.getString("Artist");
-            String year = result.getString("Year");
+            int id = result.getInt("id");
+            String name = result.getString("name");
+            String artist = result.getString("artist");
+            String year = result.getString("year");
             albums.add(new AlbumModel(id, name, artist, year));
         }
         return albums;
@@ -71,11 +71,12 @@ public class DAO {
     }
 
     private int tryToAddAlbum(Statement statement, AlbumModel album) throws SQLException {
-        int id = nextID();
-        String query = "insert into \"Albums\"" + "(\"ID\", \"Name\", \"Artist\", \"Year\")" + "values ('" + id
-                + "', '" + album.name() + "', '" + album.artist() + "', '" + album.year() + "');";
+        int newID = nextID();
+        String query = String.format("INSERT INTO album (name, artist, year) VALUES ('%s', '%s', '%s');",
+                album.name(), album.artist(), album.year());
         statement.executeUpdate(query);
-        return id;
+        // TODO Get new id from SERIAL
+        return newID;
     }
 
     public void editAlbum(AlbumModel album) {
@@ -87,8 +88,7 @@ public class DAO {
     }
 
     private void tryToEditAlbum(Statement statement, AlbumModel album) throws SQLException {
-        String query = "update \"Albums\"" + "set \"Name\" = '" + album.name() + "', \"Artist\" = '"
-                + album.artist() + "', \"Year\" = '" + album.year() + "'" + "where \"ID\" = " + album.id() + ";";
+        String query = String.format("UPDATE album SET name = '%s', artist = '%s', year = '%s' WHERE id = %d;", album.name(), album.artist(), album.year(), album.id());
         statement.executeUpdate(query);
     }
 
@@ -101,7 +101,7 @@ public class DAO {
     }
 
     private void tryToDeleteAlbum(Statement statement, int id) throws SQLException {
-        String query = "delete from \"Albums\" where \"ID\" = " + id + ";";
+        String query = String.format("DELETE FROM album WHERE id = %d;", id);
         statement.executeUpdate(query);
     }
 
@@ -117,7 +117,7 @@ public class DAO {
     }
 
     private String tryToGetFavouriteYear(Statement statement) throws SQLException {
-        String query = "select \"Year\" from \"Albums\" group by \"Year\" order by count(\"ID\") desc;";
+        String query = "SELECT year FROM album GROUP BY year ORDER BY count(id) DESC";
         ResultSet resultSet = statement.executeQuery(query);
         resultSet.next();
         return resultSet.getString(1);
